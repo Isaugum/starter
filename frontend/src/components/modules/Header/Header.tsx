681cfa04cc278d010d9b32b6e9b3b7ad48b01bfa
Domen Gorjup <domen.gorjup@specto.si>
2025-12-19 11:07:51 +0100

    updated

--- frontend/src/components/modules/Header/Header.tsx
+++ frontend/src/components/modules/Header/Header.tsx
@@ -1,16 +1,22 @@
 import { TextAlignJustify } from 'lucide-react';
-import {useNavbarModal} from "@hooks/ui/useNavbarModal.tsx";
+import { useNavbarModal } from '@hooks/ui/useNavbarModal.tsx';
+import Button from '@components/atoms/Button/Button.tsx';
+import { useLogout } from '@hooks/api/useLogout.tsx';
 
 function Header() {
-    const { openNavbarModal } = useNavbarModal();
+  const { openNavbarModal } = useNavbarModal();
+  const logout = useLogout();
 
-    return (
-        <header className="p-4 flex justify-end">
-            <div className="ml-4 cursor-pointer min-md:hidden" onClick={(e) => openNavbarModal(e)}>
-                <TextAlignJustify />
-            </div>
-        </header>
-    );
+  return (
+    <header className="p-4 flex justify-end">
+      <Button onClick={() => logout.mutate()}>
+        Logout
+      </Button>
+      <div className="ml-4 cursor-pointer min-md:hidden" onClick={(e) => openNavbarModal(e)}>
+        <TextAlignJustify />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
